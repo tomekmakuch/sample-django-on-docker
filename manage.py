e66c75215693b972eb9d236cbe2dc9f82c113287
tomek makuch <tomek.makuch@gmail.com>
2022-05-03 10:38:01 +0100

    pylint test

--- manage.py
+++ manage.py
@@ -3,17 +3,17 @@
 import os
 import sys
 
-try:
-    from django.core.management import execute_from_command_line
-except ImportError as exc:
-    raise ImportError(
-        "Couldn't import Django. Are you sure it's installed and "
-        "available on your PYTHONPATH environment variable? Did you "
-        "forget to activate a virtual environment?"
-    ) from exc
-
 
 def main():
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
