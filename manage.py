e613e5f81f0db75ed592e4968372a24fc7d99c5b
tomek makuch <tomek.makuch@gmail.com>
2022-05-03 10:46:07 +0100

    pylint rc : add import-outside-toplevel

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    # pylint: disable=import-outside-toplevel
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
